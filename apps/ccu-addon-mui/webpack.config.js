5236bf7b569cbe522008d7e151136f6415383c47
Tristan Teufel <tristanteufel@googlemail.com>
2023-12-09 12:47:38 +0100

    add PWA capabilities

--- apps/ccu-addon-mui/webpack.config.js
+++ apps/ccu-addon-mui/webpack.config.js
@@ -1,8 +1,41 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const logo = path.resolve('apps/ccu-addon-mui/src/assets/logo.png');
+
 module.exports = (config, context) => {
   config.module.rules.push({
     test: /\.tcl$/,
     type: 'asset/source',
   });
+
+  config.plugins.push(
+    new WebpackPwaManifest({
+      name: 'HomeMatic CCU Addon MUI',
+      short_name: 'CCU Addon MUI',
+      description: 'HomeMatic CCU Addon MUI',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: logo,
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons', 'ios'),
+          ios: true
+        },
+        {
+          src: path.resolve('apps/ccu-addon-mui/src/assets/hmip-advanced-routing-kv.jpg'),
+          size: '1024x1024',
+          destination: path.join('icons', 'ios'),
+          ios: 'startup'
+        },
+        {
+          src: logo,
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+          destination: path.join('icons', 'android')
+        }
+      ]
+    })
+  );
 
   return config;
 };
