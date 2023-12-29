14f47b047373f952a37ff68dff8c87c650958d1e
Tristan Teufel <tristanteufel@googlemail.com>
2023-12-29 10:17:02 +0100

    create changelog based on latest commits

---
+++ .github/scripts/get_commits.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set GIT_PAGER to cat to disable paging
+export GIT_PAGER=cat
+
+# Get the last release tag
+tag=$(curl --silent "https://api.github.com/repos/firsttris/ccu-addon-mui/releases/latest" | jq -r .tag_name)
+
+# Get the SHA of the last release
+sha=$(git rev-list -n 1 $tag)
+
+# Get the commits since the last release
+git log --pretty=format:"%h %s" $sha..HEAD
