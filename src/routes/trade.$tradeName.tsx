839868a0433388b4f9f1addd28b0ba58ba9727be
Tristan Teufel <tristanteufel@googlemail.com>
2025-11-12 19:22:07 +0100

    remove leftover

--- src/routes/trade.$tradeName.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { Trade } from '../views/Trade';
-
-export const Route = createFileRoute('/trade/$tradeName')({
-  component: Trade,
-});
