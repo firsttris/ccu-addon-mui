306c356fd6a5e587c1470b8d0da926614f0b14e1
Tristan Teufel <tristanteufel@googlemail.com>
2026-01-08 08:22:45 +0100

    dont remove go:embed

--- go-server/pkg/rega/scripts.go
+++ go-server/pkg/rega/scripts.go
@@ -4,13 +4,17 @@ import (
 	_ "embed"
 )
 
-
+//go:embed scripts/get_rooms.tcl
 var getRoomsScript string
 
+//go:embed scripts/get_trades.tcl
 var getTradesScript string
 
+//go:embed scripts/get_channels_for_room.tcl
 var getChannelsForRoomScript string
 
+//go:embed scripts/get_channels_for_trade.tcl
 var getChannelsForTradeScript string
 
+//go:embed scripts/set_datapoint.tcl
 var setDatapointScript string
